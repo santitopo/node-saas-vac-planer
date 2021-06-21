798bf31715bc7dce1302a83bf0f367fa042a45c2
Mauro <maurominetti8@gmail.com>
2021-06-21 20:37:21 -0300

    query models

--- VacPlanner/index.js
+++ VacPlanner/index.js
@@ -1,8 +1,11 @@
 const CountryDataAccess = require("./dataAccess/DataAccess");
 const ConfigApi = require("./apis/ConfigApi");
+const VacQueryApi = require("./apis/VacQueryApi");
 const ConfirmReservationService = require("./controller/ConfirmReservationService")
+const QueryDataAccess = require("./dataAccess/QueryDataAccess");
 
 const countryDataAccess = new CountryDataAccess();
+const queryDataAccess = new QueryDataAccess();
 const confirmReservationService = new ConfirmReservationService();
 const configApi = new ConfigApi(countryDataAccess);
-
+const vacQueryApi = new VacQueryApi(queryDataAccess);
